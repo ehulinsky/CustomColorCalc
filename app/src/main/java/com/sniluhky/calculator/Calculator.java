5b239c215ec1387b1c52f45179e0cdecd17d0738
Michael Hulinsky <ethan.hulinsky@gmail.com>
2017-06-25 08:51:31 -0500

    Initial commit

---
+++ app/src/main/java/com/sniluhky/calculator/Calculator.java
@@ -0,0 +1,67 @@
+package com.sniluhky.calculator;
+
+/**
+ * Created by Hulinsky Family on 6/23/2017.
+ */
+
+public class Calculator {
+
+    public void putNumber(long num) {
+        firstNum=false;
+        doOperation(operation,num);
+    }
+
+    public void setOperation(Operation op)
+    {
+        //only let users enter operation once they have entered the first number
+        if(!firstNum) {
+            operation = op;
+        }
+
+    }
+    public enum Operation {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        SET
+    }
+
+    public long getResult() {
+        return result;
+    }
+
+    public void clear() {
+        result =0;
+        firstNum=true;
+        operation=Operation.SET; //next number they enter will be initial value of result
+
+    }
+
+    private void doOperation(Operation op,long num) {
+        switch (op)
+        {
+            case ADD:
+                result+=num;
+                break;
+            case SUBTRACT:
+                result-=num;
+                break;
+            case MULTIPLY:
+                result*=num;
+                break;
+            case DIVIDE:
+                result/=num;
+                break;
+            case SET:
+                result=num;
+                break;
+            default:
+                break;
+
+        }
+    }
+    private boolean firstNum=true;
+    private long result =0;
+    private Operation operation=Operation.SET;
+}
